7bb4363f03b169e96f860a39f1fc2577ef8fc63d
kantrveysel <41362595+kantrveysel@users.noreply.github.com>
2021-03-05 16:53:14 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,28 +13,34 @@ CAN can1(PD_0, PD_1, 500000);
 CANOpen CO(can1, 1);
 
 int Torq;
+int voltage;
 
 int main(){
 
-    t.start(callback(&queue, &EventQueue::dispatch_forever)); // heart-beat
-    queue.call_every(CO.syncTime, CO.syncCanOpen); 
+    t.start(callback(&queue, &EventQueue::dispatch_forever)); // Setup for SYNC
+    queue.call_every(CO.syncTime, CO.syncCanOpen); // Starts SYNC , it is necessary for reading PDO
     
-    CANMessage msg;
+    CANMessage msg; // Create CANMessage for reading
     while(true){
-        if (can1.read(msg)) {
+        
+        readSDO(0x6040, 0x0, voltage); // main index 0x6040 , sub index 0x0 , save to voltage
+        
+        if (can1.read(msg)) { // is there any data
 
-            CO.syncCanOpen();
-
-
-            CO.readPDO(msg, 1, 0, 1, Torq);
-            CO.requestSDO(0x6040, 0x40);
-            led1 != led1;
-            if(CO.readError(msg) != 0x0){
+            CO.readPDO(msg, 1, 0, 2, Torq); // Actual Torq is readable from RPDO1 index 0 and 2 byte length
+            led1 != led1; // Show me there is a data
+            if(CO.readError(msg) != 0x0){ // if there is no error then return 0x0 or return error
             
                 printf("! ERROR ! %#X \n", CO.readError(msg));
-                led3 != led3;
+                led3 != led3; // RED Light
                 
             }
-    }
+        }else{
+            
+                led2 != led2; // No data on CAN
+                
+         }
+        
+        printf(" Voltage : %d\n Torque : %d\n", voltage, Torq); // All data we collect
     }
 }
