d781e4f218325a688c42963c06c9b780097ce392
kantrveysel <41362595+kantrveysel@users.noreply.github.com>
2021-03-05 15:07:54 +0300

    Create CANOpen.h

---
+++ CANOpen/CANOpen.h
@@ -0,0 +1,33 @@
+#include "mbed.h"
+
+#define T_SDO_CAN_ID            0x600
+#define R_SDO_CAN_ID            0x580
+
+using duration = std::chrono::duration<int, std::milli>;
+
+class CANOpen{
+
+    private:
+        CAN &can;
+        char motorNodeID = 10;
+        duration syncTime = 1s;
+        int _currentData;
+        char _NodeId,Res;
+
+    public:
+        CANOpen(CAN &_can, char _NodeId);
+        
+        char ControllerTemp, MotorTemp;
+        int ControlWord=0x0F, TargetVelocity, TargetTorque, TargetPosition, StatusWord;
+        int PositionActualValue, TorqueActualValue, DCLinkVoltage, LogicPowerSupplyVoltage;
+        int CurrentDemand, MotorCurrentActualValue, ElectricalAngle, PhaseACurrent, PhaseBCurrent;
+        
+        void requestSDO(int main_index, int sub_index);
+        void setSDO(int main_index, int sub_index, int data);
+        int * signed16(int x);
+        void syncCanOpen();
+        void updatePDO(char pdo);
+        void writePDO(char pdo, char data[]);
+        void readPDO(CANMessage msg, char pdo, char startByte, char size, int &a);
+
+};
